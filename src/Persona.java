150be3c622fee60bb8a0de7bf988f51a920e0a34
penaherrera <carlos.cpa26@gmail.com>
2024-02-18 20:34:07 -0600

    Adding methods into persona archive

--- src/Persona.java
+++ src/Persona.java
@@ -1,2 +1,28 @@
 public class Persona {
+    //private instance variables
+    private String nombre;
+    private int edad;
+
+    public Persona(String nombre, int edad){
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+    //getter for nombre
+    public String getNombre(){
+        return nombre;
+    }
+
+    //setter for nombre
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public void setanoiNac(int anioNac){
+        this.edad = anioNac - 2024;
+    }
+
+    public int getEdad(){
+        return edad;
+    }
+
 }
