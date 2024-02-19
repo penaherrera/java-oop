22e942be8bb5312fdf12019503f40ac50ee24180
penaherrera <carlos.cpa26@gmail.com>
2024-02-18 20:33:50 -0600

    Adding methods into persona archive

--- src/Paciente.java
+++ src/Paciente.java
@@ -1,2 +1,17 @@
-public class Paciente {
+
+public class Paciente extends Persona{
+
+    String diagnostico;
+    public Paciente(String nombre, int edad, String diagnostico) {
+        super(nombre, edad);
+        this.diagnostico = diagnostico;
+    }
+
+    public void setDiagnostico(String diagnostico) {
+        this.diagnostico = diagnostico;
+    }
+
+    public String getDiagnostico() {
+        return diagnostico;
+    }
 }
