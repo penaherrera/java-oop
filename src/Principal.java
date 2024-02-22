dbacfdcab09186a06bf09ee1950e69a550ae300d
penaherrera <carlos.cpa26@gmail.com>
2024-02-22 17:48:22 -0600

    Adding main logic to run program

--- src/Principal.java
+++ src/Principal.java
@@ -1,6 +1,34 @@
 import javax.swing.JOptionPane;
 public class Principal {
-    public static void Main(String[] args){
-        Medico DrHouse = new Medico("DrHouse", 45);
+    public static void main(String[] args){
+        String nombre = JOptionPane.showInputDialog("Ingrese su nombre:");
+        String inputEdad = JOptionPane.showInputDialog("Ingrese su edad:");
+        int edad = Integer.parseInt(inputEdad);
+        String especialidad = JOptionPane.showInputDialog("Inregese su especialidad:");
+
+        Medico DrHouse = new Medico(nombre, edad, especialidad);
+        String mensajeIdentificacion = "Usuario identificado como:\n" + "Nombre: " + nombre
+                + "\n" + "Edad: " + edad + "\n" + "Especialidad: " + especialidad;
+
+        JOptionPane.showMessageDialog(null, mensajeIdentificacion);
+
+        JOptionPane.showMessageDialog(null,"Por favor ingrese los datos de el " +
+                "paciente que desea registrar");
+
+        String nombrePaciente = JOptionPane.showInputDialog("Ingrese su nombre:");
+        String inputEdadPaciente = JOptionPane.showInputDialog("Ingrese su edad:");
+        int edadPaciente = Integer.parseInt(inputEdad);
+        String especialidadPaciente = JOptionPane.showInputDialog("Inregese su diagnostico:");
+
+        Paciente paciente1 = new Paciente(nombrePaciente, edadPaciente, especialidadPaciente);
+        Paciente paciente2 = new Paciente("Ejemplo", 40, "Exceso de programacion");
+        DrHouse.pacientes.add(paciente2);
+        DrHouse.pacientes.add(paciente1);
+
+        JOptionPane.showMessageDialog(null,"Paciente registrado con exito! \n" +
+                "A continuacion su listado de pacientes:");
+
+        DrHouse.verExpediente(paciente1);
+        DrHouse.verExpediente(paciente2);
     }
 }
